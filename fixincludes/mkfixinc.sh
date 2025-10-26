37c34f3c27e88204ffb96f542f4fe626d6f8eb0a
Jackson Brenneman <jdb360@georgetown.edu>
2025-10-26 10:32:27 -0400

    add files for 15.2 release

--- fixincludes/mkfixinc.sh
+++ fixincludes/mkfixinc.sh
@@ -11,6 +11,8 @@ target=fixinc.sh
 
 # Check for special fix rules for particular targets
 case $machine in
+    *-lilac* | \
+    *-*-lilac* | \
     i?86-*-cygwin* | \
     *-mingw32* | \
     powerpc-*-eabisim* | \
